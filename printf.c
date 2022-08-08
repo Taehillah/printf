b700c558ac57bdb28abab2f86372473dc13b7ef1
Taehillah <ishmaelmafole@gmail.com>
2022-08-08 06:36:08 -0700

    Task 0. Ammendments

--- printf.c
+++ printf.c
@@ -8,18 +8,20 @@
  * Return: return a pointer to a function.
  * Or return: NULL if the function points to nothing.
  */
-int (*insp_format(const char *format))(va_list)
+static int (*insp_format(const char *format))(va_list)
 {
-int index = 0;
+unsigned int index;
 print_t p[] = {
-	{"c", print_c},
-	{"s", print_s},
-	{NULL, NULL}
-	};
+  {"c", print_c},
+  {"s", print_s},
+  {NULL, NULL}
+  };
 for (index = 0; p[index].x != NULL; index++)
 {
-	if (*(p[index].x) == *format)
-		break;
+  if (*(p[index].x) == *format){
+    break;
+  }
+    
 }
 return (p[index].y);
 }
@@ -33,40 +35,38 @@ int _printf(const char *format, ...)
 va_list sb;
 int (*y)(va_list);
 unsigned int i = 0, cnt = 0;
-	if (format == NULL)
-		return (-1);
+  if (format == NULL)
+    return (-1);
 va_start(sb, format);
-{
-	while (format && format[i])
-	{
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			cnt++;
-			continue;
-		}
-		else
-		{
-			if (format[i + 1] == '%')
-			{
-			_putchar('%');
-			cnt++;
-			i += 2;
-			continue;
-			}
-			else
-			{
-			y = insp_format(&format[i + 1]);
-			if (y == NULL)
-			return (-1);
-			i += 2;
-			cnt += y(sb);
-			continue;
-			}
-		}
-		i++;
-	}
-	va_end(sb);
-	return (cnt);
-}
+  while(format[i])
+  {
+      for (;format[i] !='%' && format[i]; i++)
+      {
+        _putchar( format[i]);
+        cnt++;
+      }
+      
+      if (format[i])
+        return(cnt);
+    y = insp_format(&format[i + 1]);
+    if (y != NULL)
+    {
+        cnt += y(sb);
+        i+=2;
+        continue;
+    }
+    
+      if (!format[i + 1])
+      {
+      return (-1);
+      _putchar(format[i]);
+        cnt++;
+        if(format[i+1] == '%')
+        {i+=2;}
+        else
+        {i++;} 
+      }
+  }
+  va_end(sb);
+  return (cnt);
 }
