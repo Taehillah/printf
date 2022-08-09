40f7f45a2d9be6608b390796f702c314dfebd626
Taehillah <ishmaelmafole@gmail.com>
2022-08-09 10:14:10 -0700

    betty fixed

--- main.h
+++ main.h
@@ -3,17 +3,29 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- * struct print - The structure for different types
- * of variables.
- * @x: Variable type to be printed.
- * @y: Function pointer to print that type.
- */
-typedef struct print {
-  char *x;
-  int (*y)(va_list);
-} print_t;
+* struct print - The structure for different types
+* of variables.
+* @x: Variable type to be printed.
+* @y: Function pointer to print that type.
+*/
+typedef struct print
+{
+char *x;
+int (*y)(va_list);
+}
+print_t;
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_c(va_list c);
 int print_s(va_list s);
+int print_p(va_list p);
+int print_d(va_list d);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_o(va_list o);
+int print_u(va_list u);
+int print_r(va_list r);
+int print_R(va_list R);
+int print_b(va_list b);
+int print_i(va_list i);
 #endif
