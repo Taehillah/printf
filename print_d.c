0dc74854657cf40183d4a691116fa5d3bc3700e1
Taehillah <ishmaelmafole@gmail.com>
2022-08-09 11:35:23 -0700

    Adding even files

--- print_d.c
+++ print_d.c
@@ -19,6 +19,34 @@ cnt++;
 p[0] = y / x;
 for (; index < 10; index++)
 {
+x /= 10;
+p[index] = (y / x) % 10;
+}
+for (index = 0; index < 10; index++)
+{
+total += p[index];
+if (total != 0 || index == 9)
+{
+_putchar('0' + p[index]);
+cnt++;
+}
+}
+return (cnt);
+}
+(va_list i)
+{
+int p[10];
+int index = 1, x = 1000000000, y, total = 0, cnt = 0;
+y = va_arg(i, int);
+if (y < 0)
+{
+y *= -1;
+_putchar('-');
+cnt++;
+}
+p[0] = y / x;
+for (; index < 10; index++)
+{
 x / = 10;
 p[index] = (y / x) % 10;
 }
